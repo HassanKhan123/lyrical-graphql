6d7b817c0285844748f39bc368fcb7cfecd98d30
HassanKhan123 <hk2602809@gmail.com>
2021-05-25 21:48:03 +0500

    delete song functionality added

--- client/src/components/SongList.js
+++ client/src/components/SongList.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 import fetchSongs from '../queries/fetchSongs';
 
 const SongList = () => {
   const query = fetchSongs;
-
   const { data, loading, error } = useQuery(query);
+
+  const DELETE_SONG = gql`
+    mutation DeleteSong($id: ID) {
+      deleteSong(id: $id) {
+        id
+      }
+    }
+  `;
+
+  const [deleteSong] = useMutation(DELETE_SONG);
+
+  const deleteHandler = async (id) => {
+    try {
+      await deleteSong({
+        variables: { id },
+        // refetchQueries: [{ query: fetchSongs }],
+      });
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
 
   const renderSongs = () => {
     return (
       data &&
-      data.songs.map((song) => {
+      data.songs.map(({ id, title }) => {
         return (
-          <li key={song.id} className='collection-item'>
-            {song.title}
+          <li key={id} className='collection-item'>
+            {title}
+            <i className='material-icons' onClick={() => deleteHandler(id)}>
+              delete
+            </i>
           </li>
         );
       })
