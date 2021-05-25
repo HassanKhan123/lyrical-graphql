6020f9bc90fd67c946bf9a667419f740f553a281
HassanKhan123 <hk2602809@gmail.com>
2021-05-25 20:55:11 +0500

    query variables added

--- client/src/components/SongCreate.js
+++ client/src/components/SongCreate.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 
 const SongCreate = () => {
   const [title, setTitle] = useState('');
+
+  const ADD_SONG = gql`
+    mutation AddSong($title: String) {
+      addSong(title: $title) {
+        title
+      }
+    }
+  `;
+
+  const [addSong] = useMutation(ADD_SONG);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    addSong({ variables: { title } });
+    setTitle('');
+  };
   return (
     <div className='container'>
       <h3>Create a New Song!</h3>
-      <form>
+      <form onSubmit={submitHandler}>
         <label>Song Title:</label>
         <input value={title} onChange={(e) => setTitle(e.target.value)} />
       </form>
